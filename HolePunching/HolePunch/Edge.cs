d673e8b370bce47a8a1f6b84afc4bec1e47cfb7e
computersmoke <addison@assurancefamily.com>
2023-11-16 23:09:50 -0600

    Updated to stride 4.2, worked on implementing polygon intersection stuff

---
+++ HolePunching/HolePunch/Edge.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+
+namespace HolePunching.HolePunch
+{
+    internal class Edge (Vector2 v1, Vector2 v2)
+    {
+        public Vector2 v1 = v1;
+        public Vector2 v2 = v2;
+        protected Vector2 segment = v2 - v1;
+
+        public Edge Intersect(Edge other)
+        {
+            if (!InBoundingBox(other))
+                return null;
+            if (Parallel(other))
+                return ParallelIntersect(other);
+            //TODO: check for intersection in non parallel case
+        }
+        private bool InBoundingBox(Edge other)
+        {
+            return Intersect1D(v1.X, v2.X, other.v1.X, other.v2.X) && Intersect1D(v1.Y, v2.Y, other.v1.Y, other.v2.Y);
+        }
+        private static bool Intersect1D(float a1, float a2, float b1, float b2)
+        {
+            if (a1 > a2)
+                (a1, a2) = (a2, a1);
+            if (b1 > b2)
+                (b1, b2) = (b2, b1);
+            return Math.Max(a1, b1) <= Math.Min(a2, b2);
+        }
+        private Edge ParallelIntersect(Edge other)
+        {
+            Vector2 q1 = other.v1;
+            Vector2 q2 = other.v2;
+            if(Vector2.Dot(other.v1, segment) > Vector2.Dot(other.v2, segment))
+                (q2, q1) = (q1, q2);
+            Vector2 w1 = v1;
+            if (Vector2.Dot(q1, segment) > Vector2.Dot(v1, segment))
+                w1 = q1;
+            Vector2 w2 = v2;
+            if(Vector2.Dot(q2, segment) < Vector2.Dot(v2, segment))
+                w2 = q2;
+            return new Edge(w1, w2);
+        }
+        protected bool Parallel(Edge other)
+        {
+            return Math.Abs(segment.X * other.segment.Y) == Math.Abs(segment.Y * other.segment.X);
+        }
+        public bool IsPoint()
+        {
+            return v1 == v2;
+        }
+    }
+}
---
+++ HolePunching/HolePunch/Polygon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.MediaFoundation;
+using Stride.Core.Mathematics;
+
+namespace HolePunching.HolePunch
+{
+    internal class Polygon(Vector2[] vertices)
+    {
+        //vertices in clockwise order
+        public readonly Vector2[] vertices = vertices;
+        //perps are normal to edges, but may not have magnitude 1
+        private readonly Vector2[] perps = Perpendiculars(vertices);
+
+        //True if point is strictly inside polygon
+        public bool Contains(Vector2 point)
+        {
+            for(int i = 0; i < vertices.Length; i++)
+            {
+                Vector2 v1 = vertices[i];
+                Vector2 normal = perps[i];
+                float dist = Vector2.Dot(normal, point - v1);
+                if(dist <= 0)
+                    return false;
+            }
+            return true;
+        }
+        private static Vector2[] Perpendiculars(Vector2[] vertices)
+        {
+            Vector2[] normals = new Vector2[vertices.Length-1];
+            for(int i = 0; i < vertices.Length; i++)
+            {
+                Vector2 v1 = vertices[i];
+                Vector2 v2 = vertices[(i+1)%vertices.Length];
+                Vector2 edge = v2 - v1;
+                //normal is edge rotated 90 degrees clockwise, length normalized
+                normals[i] = new Vector2(edge.Y, -edge.X);
+            }
+            return normals;
+        }
+    }
+}
